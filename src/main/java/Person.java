bea1626ac2b2e189bd5ff19a0c96dce5b7c5a43b
Clyde Broderick <Clyde@returnofthemac.myfiosgateway.com>
2022-07-11 17:51:35 -0400

    Added Person class.

---
+++ src/main/java/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    String firstName;
+    String lastName;
+    String idNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+}
